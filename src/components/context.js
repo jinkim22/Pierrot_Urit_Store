547f17c0abbc8cd6ef5ea1e6f03542e065e0e851
Jinhyuk Kim <jikim@g.hmc.edu>
2020-02-12 01:18:26 -0800

    left off at 3:25:08, implemented id led switching of products in item details

--- src/components/context.js
+++ src/components/context.js
@@ -7,15 +7,37 @@ const ProductContext = React.createContext()
 
 export default class ProductProvider extends Component {
     state ={
-        products:storeProducts, 
-        detailProduct:detailProduct
+        products: [], 
+        detailProduct: detailProduct
+    };
+    componentDidMount() {
+        this.setProducts();
+    };
+    setProducts = () => {
+        let tempProducts = [];
+        storeProducts.forEach(item => {
+            const singleItem = {...item};
+            tempProducts = [...tempProducts, singleItem]; 
+        });
+        this.setState(()=>{
+            return {products: tempProducts}
+        });
+    };
+
+    getItem = (id) => {
+        const product = this.state.products.find(item => item.id === id);
+        return product;
     }
-    handleDetail = () => {
-        console.log('hello from detail');
-    }
-    addToCart = () =>{
-        console.log('hello from add to cart');
-    }
+    handleDetail = (id) => {
+        const product = this.getItem(id);
+        this.setState(()=>{
+            return {detailProduct: product};
+        });
+    };
+    addToCart = (id) =>{
+        console.log(`hello from add to card. id is ${id}`);
+    };
+    
     render() {
         return (
         <ProductContext.Provider 
@@ -23,9 +45,10 @@ export default class ProductProvider extends Component {
             ...this.state, 
             handleDetail: this.handleDetail,
             addToCart: this.addToCart
-        }}>
+        }}
+        >
             {this.props.children}
-        </ProductContext.Provider>
+            </ProductContext.Provider>
     )
  }
 }
